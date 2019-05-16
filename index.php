b4fe1fc8fa17266db36b2172fe29291428b61802
banditsmile <banditsmile@qq.com>
2019-05-16 11:56:49 +0800

    dns记录查询服务

---
+++ index.php
@@ -0,0 +1,123 @@
+<?php
+
+require 'autoload.php';
+
+/**
+ * Class index
+ */
+class index{
+    private $_param = array();
+
+    /**
+     * index constructor.
+     */
+    public function __construct($argv)
+    {
+        if (PHP_SAPI == 'cli') {
+            $action = $argv[1];
+            $this->_param = array_slice($argv, 2);
+        } else {
+            $this->_param = $_REQUEST;
+            $action = $this->_get('a', 'query');
+        }
+
+        if (!method_exists($this, $action)) {
+            $this->_out([], 3, '行为参数错误');
+        }
+        return $this->$action();
+    }
+
+    /**
+     * 获取参数
+     *
+     * @param      $key
+     * @param null $default
+     *
+     * @return mixed|null
+     */
+    private function _get($key, $default=null)
+    {
+        if (isset($this->_param[$key])) {
+            return $this->_param[$key];
+        }
+        return $default;
+    }
+
+    /**
+     * 输出结果
+     *
+     * @param array  $data
+     * @param int    $code
+     * @param string $msg
+     *
+     */
+    private function _out($data=array(), $code=0, $msg='ok')
+    {
+        $ret=array(
+            'code'=>$code,
+            'msg' =>$msg,
+            'data'=>$data
+        );
+        echo json_encode($ret);
+        exit();
+    }
+
+    /**
+     * 记录查询
+     *
+     * @param string $dns dns '223.5.5.5
+     * @param string $domain 查询域名
+     * @param string $type   查询类型
+     *
+     * @return null
+     */
+    public function query()
+    {
+        $data = array('code'=>0, 'msg'=>'ok');
+        $dnsServer = isset($_GET['dns']) ? $_GET['dns']:'223.5.5.5';
+        if (!isset($_GET['domain'])) {
+            $this->_out([], 1, '域名参数不能为空');
+        }
+        $domain = $_GET['domain'];
+        $queryType = isset($_GET['type']) ? $_GET['type']:'A';
+
+        try{
+            $dns = new Metaregistrar\DNS\dnsProtocol();
+            $dns->setServer($dnsServer);
+            $result = $dns->Query($domain, $queryType);
+        }catch (Exception $e){
+            $this->_out([], 2, $e->getMessage());
+        }
+
+        /* @var $result Metaregistrar\DNS\dnsResponse */
+        foreach ($result->getResourceResults() as $resource) {
+            if ($resource instanceof Metaregistrar\DNS\dnsAresult) {
+                $data[]=array(
+                    'domain'=>$resource->getDomain(),
+                    'ipv4'  =>$resource->getIpv4(),
+                    'ttl'   =>$resource->getTtl()
+                );
+            }
+        }
+        $this->_out($data);
+    }
+
+    public function rootzone()
+    {
+        $zoneFile = 'https://www.internic.net/domain/root.zone';
+        $ns = array();
+        $rootzone = file($zoneFile, FILE_IGNORE_NEW_LINES);
+        foreach ($rootzone as $zone) {
+            $array = explode("\t", $zone);
+            if (isset($array[2]) && $array[2]=='IN' && $array[3]=='NS') {
+                $tld = strtolower($array[0]);
+                if ($tld) {
+                    $tld=trim($tld, '.');
+                    $ns[$tld][] = $array[4];
+                }
+            }
+        }
+        $this->_out($ns);
+    }
+}
+$instance = new index($argv);
--- rootzone.php
+++ rootzone.php
@@ -1,46 +1,16 @@
 <?php
+
+$zoneFile = 'https://www.internic.net/domain/root.zone';
 $ns = array();
-$rootzone = file('https://www.internic.net/domain/root.zone',FILE_IGNORE_NEW_LINES);
-foreach ($rootzone as $zone){
-    $array = explode("\t",$zone);
-    if (isset($array[4])) {
-        if ($array[4]=='IN') {
-            if ($array[5]=='NS') {
-                $type = $array[5];
-                $tld = strtolower($array[0]);
-                if (strlen($tld)>1) {
-                    if ($tld{strlen($tld)-1}=='.') {
-                        $tld = substr($tld,0,strlen($tld)-1);
-                    }
-                    $content = $array[6];
-                    $ns[$tld][]=$content;
-                }
-
-
-            }
-
+$rootzone = file($zoneFile, FILE_IGNORE_NEW_LINES);
+foreach ($rootzone as $zone) {
+    $array = explode("\t", $zone);
+    if (isset($array[2]) && $array[2]=='IN' && $array[3]=='NS') {
+        $tld = strtolower($array[0]);
+        if ($tld) {
+            $tld=trim($tld, '.');
+            $ns[$tld][] = $array[4];
         }
     }
 }
-echo 'private $ns = array('."\n";
-$tldcount = count($ns);
-$tldteller = 0;
-foreach ($ns as $index=>$tld) {
-    echo "'".$index."'=>array(";
-    $count = count($tld);
-    $teller = 0;
-    foreach ($tld as $content) {
-        echo "'".$content."'";
-        $teller++;
-        if ($teller < $count) {
-            echo ',';
-        }
-    }
-    echo ')';
-    $tldteller++;
-    if ($tldteller < $tldcount) {
-        echo ',';
-    }
-    echo"\n";
-}
-echo ");";
+var_export($ns);
